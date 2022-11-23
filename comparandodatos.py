0bf843379300f48149f8d416cf8470f81ba27d5b
cesdeleanmuag <115880909+cesdeleanmuag@users.noreply.github.com>
2022-11-23 10:31:59 -0600

    se crean ejemplos de variables y condiciones

---
+++ comparandodatos.py
@@ -0,0 +1,12 @@
+#entradas
+edadUsuario=int(input("Digita tu edad: "))
+saldoUsuario=int(input("Digita cuanto dinero traes contigo: "))
+
+#proceso
+#pregunto por la edad
+if edadUsuario >= 18:
+    print("usted es mayor de edad")
+else:
+    print("usted es menor de edad")
+
+print("Discoteca PERRO NEGRO LTDA\n")
