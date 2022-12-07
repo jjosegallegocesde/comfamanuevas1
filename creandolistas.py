f57fe8ba755de299013ef6657607eff69b649fa9
cesdeleanmuag <115880909+cesdeleanmuag@users.noreply.github.com>
2022-12-07 10:28:41 -0600

    se crean listas

---
+++ creandolistas.py
@@ -0,0 +1,17 @@
+#una lista es una variable
+
+numeros=[1,2,3,4,5]
+nombres=["Juan","Sara","Carlos","Martha"]
+
+'''print(numeros)
+print(numeros[4])'''
+
+#Recorrer una lista
+
+#FOR CON UN RANGO
+'''for i in range(len(nombres)):
+    print(nombres[i])'''
+
+#FOR CON UNA LISTA
+for nombre in nombres:
+    print(nombre)
---
+++ reto3.py
@@ -0,0 +1,6 @@
+numeros = []
+for numero in range(20):
+    numeros.append(input("ingrese un numero: "))
+
+for numero in numeros:
+    print(numero)
